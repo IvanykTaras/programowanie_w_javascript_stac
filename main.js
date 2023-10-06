b6d2eaf77cf1990b80c104264efd01ca305d4d19
unknown <Taras.Ivanyk@microsoft.wsei.edu.pl>
2023-10-06 13:12:39 +0200

    initial

---
+++ main.js
@@ -0,0 +1,43 @@
+const btnPrzelicz = document.querySelector('#przelicz')
+const wynikiPojemnik = document.querySelector('#wyniki')
+
+const input_fields = document.querySelector(".input_fields");
+const add_button = document.querySelector("#add");
+const arr_fields = [];
+
+createElement(input_fields);
+createElement(input_fields);
+createElement(input_fields);
+
+function createElement(parent){
+    const div = document.createElement("div");
+    const input = document.createElement("input")
+    const button = document.createElement("button")
+    
+    div.classList.add("input_field");
+    
+    input.type = "number";
+    
+    button.textContent = "-"
+    button.addEventListener("click",()=>deleteElement(parent,div))
+
+
+
+    
+    div.appendChild(button);
+    div.appendChild(input);
+
+
+    arr_fields.push(div)
+    parent.appendChild(div);
+}
+
+function deleteElement(parent,element) {
+    parent.removeChild(element);
+}
+
+
+
+
+btnPrzelicz.addEventListener("click",()=>MathTask());
+add_button.addEventListener("click", ()=>createElement(input_fields))
