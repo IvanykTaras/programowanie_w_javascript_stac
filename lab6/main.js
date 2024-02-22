95b6272d9234ddcd36a29a8c9c8598d96f51c2e7
TarasIvanyk <hanekava4@gmail.com>
2024-02-22 13:59:48 +0100

    lab6 done

--- lab6/main.js
+++ lab6/main.js
@@ -1,33 +1,148 @@
-let c = document.getElementById("canvas");
-let ctx = c.getContext("2d");
-const alpha = {min: -180, max: 179}
-const beta = {min: -180, max: 179}
-const gamma = {min: -90, max: 89}
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-class Circle{
-    x = 0;
-    y = 0;
-    r = 20;
-    fill = true;
+window.onload = ()=>{
+    if(localStorage.getItem("highScore") === null){
+        localStorage.setItem("highScore", 0)
+    }
+    draw();
+    high_score.innerHTML = `High score: ${Number(localStorage.getItem("highScore"))}`
+};
 
-   draw(){
-    if(this.fill){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        ctx.fill();
-    }else{
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        ctx.stroke();
-    } 
-   } 
+//Timer part
+let startTime;
+let isRunning = false;
+let requestId;
+const timerElement = document.getElementById('timer');
+const scoreElement = document.getElementById('score');
+const high_score = document.getElementById('high_score');
+
+function startTimer() {
+    if (!isRunning) {
+        isRunning = true;
+        startTime = performance.now();
+        updateTimer();
+    }
 }
 
-const circle1 = new Circle();
-circle1.draw();
+function stopTimer() {
+    isRunning = false;
+    cancelAnimationFrame(requestId);
+}
+
+function updateTimer() {
+    //stop timer after 60 seconds
+    
+    if (!isRunning) return;
+    const elapsedTime = performance.now() - startTime;
+    let seconds = Math.floor(elapsedTime / 1000);
+    
+    timerElement.innerHTML = `Timer: ${seconds} seconds`;
+    scoreElement.innerHTML = `Score: ${numOfScore}`;
+    high_score.innerHTML = `High score: ${Number(localStorage.getItem("highScore"))}`
+
+    draw()
+    requestId = requestAnimationFrame(updateTimer);
+
+    if(seconds >= 60){
+        startTime = 0;
+        seconds = 0;
+        stopTimer()
+        numOfScore = 0;
+        alert("Game is finish");
+    } 
+}
 
 
-window.addEventListener("deviceorientation",(a,b,y)=>{
-    console.log(a,b,y);
+const startButton = document.querySelector("#start");
+const stopButton = document.getElementById("stop");
+startButton.addEventListener("click",()=>{
+    startTimer()
+    x = Math.floor(Math.random() * canvas.width) + 1;
+    y = Math.floor(Math.random() * canvas.height) + 1;
+})
+stopButton.addEventListener("click",()=>{
+    stopTimer()
+    numOfScore = 0;
 })
 
+
+
+let x = Math.floor(Math.random() * canvas.width) + 1;
+let y = Math.floor(Math.random() * canvas.height) + 1;
+let numOfScore = 0;
+let ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#ff0000";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawHole() {
+    ctx.beginPath();
+    ctx.arc(canvas.width/2, canvas.height/2, ballRadius, 0, Math.PI*2);
+    ctx.strokeStyle = "#0000FF";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+    clearCanvas();
+    drawHole();
+    drawBall();
+    score();
+    moveBall();
+}
+
+function score() {
+    if(
+        x > canvas.width/2 - ballRadius &&
+        x < canvas.width/2 + ballRadius &&
+        y > canvas.height/2 - ballRadius &&
+        y < canvas.height/2 + ballRadius 
+    ){
+        x = Math.floor(Math.random() * canvas.width) + 1;
+        y = Math.floor(Math.random() * canvas.height) + 1;
+
+        numOfScore++;
+        let storageScore = Number(localStorage.getItem("highScore"));
+        if(storageScore < numOfScore){
+            localStorage.setItem("highScore", numOfScore);
+        }
+    }
+}
+
+function moveBall() {
+    
+    console.log(a,b);
+    if((a < 0 || a > 180) && x > 0){// left move
+        x -= 1;
+
+    }if((a>0 && a < 180)  && x < canvas.width){// right move
+        x += 1;
+        
+    }if(b < 90 && y > 0){// up move
+        y -= 1;
+        
+    }if(b > 90 && y < canvas.height){// down move
+        y += 1;
+
+    }
+}
+
+let a = 0;
+let b = 90;
+function orientationHandler(e) {
+    a = e.alpha;
+    b = e.beta
+}
+
+window.addEventListener('deviceorientation', orientationHandler)
+  
